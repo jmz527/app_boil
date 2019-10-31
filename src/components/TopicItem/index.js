f5d6a07698acc9e8d7df86687779a43d3f1d0493
james rutledge <jarutledge2@gmail.com>
2019-10-31 02:00:49 -0400

    refact of TopicItem component

--- src/components/TopicItem/index.js
+++ src/components/TopicItem/index.js
@@ -1,17 +1,41 @@
 // Main Imports
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 // Style Imports
 // import './index.scss';
 
-const topicItem = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
+const topicItem = ({ match, fetching, topics }) => {
+  let title = '', id = '';
+
+  if (topics.objects[match.params.topicId]) {
+    title = topics.objects[match.params.topicId].title;
+    id = topics.objects[match.params.topicId].id;
+  }
+
+  return (
+    <div>
+      { fetching && !topics.ids.length ?
+        <h3>{match.params.topicId}</h3>
+        :
+        <h3>{`#${id}: ${title}`}</h3>
+      }
+    </div>
+  );
+};
 topicItem.propTypes = {
-  match: PropTypes.object
+  fetching: PropTypes.bool,
+  match: PropTypes.object,
+  topics: PropTypes.object
 };
 
-export default topicItem;
+const mapStateToProps = (state) => {
+  return {
+    fetching: state.ui.fetching,
+    topics: state.topics
+  };
+};
+
+export default connect(mapStateToProps)(withRouter(topicItem));
